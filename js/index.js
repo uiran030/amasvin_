e959a6c675d7e43bff6db25eed0ff4d6cecc90c8
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-01-11 16:08:49 +0900

    amasvin V5.0

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//슬릭슬라이더구역
+$('.centerInner').slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    speed:500,
+    arrow:true,
+    prevArrow:'<button class="slick-arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow : '<button class="slick-arrow slick-next"><i class="fas fa-angle-right"></i></button>',
+    dots:true,
+    fade:true
+})
